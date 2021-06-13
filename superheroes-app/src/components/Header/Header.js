221bef35226cc94569c768d16d7e5ee9d66137cc
Carlos <carlos.alfonso.montero@gmail.com>
2021-06-12 21:47:48 -0400

    delete redirects file

--- superheroes-app/src/components/Header/Header.js
+++ superheroes-app/src/components/Header/Header.js
@@ -4,11 +4,6 @@ import './Header.css';
 function Header() {
     return (
         <div className="header"> 
-        {/*<img
-          className= "heroes-header"
-          src="https://www.comicbasics.com/wp-content/uploads/2020/08/The-Top-10-Greatest-Superheroes-Without-Superpowers-In-Comics-Today-678x381.jpg"
-          alt="heroes header"
-        />*/}
         <h1 >Superheroes Page</h1>
       </div>
     );
